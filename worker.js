5a09c3812211d86cc3a5b0f696392b1357d8a47c
Maarten Coppens <martinuske@gmail.com>
2021-03-01 15:00:19 -0300

    mounted to spawned

--- worker.js
+++ worker.js
@@ -261,7 +261,7 @@ let ipcTimeout = argv['ldem-ipc-timeout'];
   })();
 
   try {
-    // Call the load function in the mounted node_module
+    // Call the load function in the spawned node_module
     await targetModule.load(channel, targetModule.options, targetModule.appConfig, appDependentMap);
   } catch (error) {
     logger.fatal(error);
